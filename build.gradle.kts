cb4ef99c1eceb53e62b778a663e1466bb2b17fa0
Vinícius Gutierrez <srgutyerrez@gmail.com>
2021-06-05 14:18:15 -0300

    dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,7 @@ version = "0.1-ALPHA"
 repositories {
     mavenCentral()
 
-    maven("http://135.148.58.224:8081/artifactory/releases/") {
-        isAllowInsecureProtocol = true
-
+    maven("https://repository.hyren.net/") {
         credentials {
             username = System.getenv("MAVEN_USERNAME")
             password = System.getenv("MAVEN_PASSWORD")
